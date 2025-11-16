9d92a12b70562a00afbbffd3f0b0463a3cb77bce
Ishaque Appiah <ishaque.appiah@amalitech.com>
2025-11-16 19:01:36 +0000

    feat: implement streaming of output from open ai

--- backend/routes/chat_routes.py
+++ backend/routes/chat_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from models.chat_models import ChatMessage, ChatResponse
-from services.open_ai_service import basic_chat_open_ai
+from services.open_ai_service import basic_chat_open_ai, streamed_chat
+from fastapi.responses import StreamingResponse
 router = APIRouter()
 
 @router.post("/chat")
@@ -11,3 +12,16 @@ async def chat(chat_message: ChatMessage) -> ChatResponse:
     
     return ChatResponse(response=response)
 
+@router.post("/chat/stream")
+async def stream_chat(chat_message:ChatMessage):
+    message = chat_message.message
+    stream_response =streamed_chat(message)
+    async def event_generator():
+     for event in stream_response:
+        if isinstance(event, dict) and 'type' in event and event['type']=='response.output_text.delta':
+            delta = event.get('delta') 
+            if delta:
+                    yield f"data: {delta}\n\n" 
+           
+        
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
--- backend/services/open_ai_service.py
+++ backend/services/open_ai_service.py
@@ -11,3 +11,12 @@ def basic_chat_open_ai(chat:str):
     conversation=conversation.id
 )
     return response.output_text
+
+def streamed_chat(chat:str):
+    stream = client.responses.create( 
+    model="gpt-5",
+    input=[{"role": "user", "content": chat}],
+    conversation=conversation.id,
+    stream=True
+)
+    return stream
